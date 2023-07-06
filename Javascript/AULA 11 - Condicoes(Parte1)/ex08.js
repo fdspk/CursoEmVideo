435585f1a5a91207cf3d64a5e1b0dca6808207e2
Fabiana Kraft <fabianadspk@gmail.com>
2023-07-06 14:17:08 -0300

    Aula 11

---
+++ Javascript/AULA 11 - Condicoes(Parte1)/ex08.js
@@ -0,0 +1,6 @@
+console.log("Hello, World! - obs: use Fn+F8 para abrir o console e atualizar-lo")
+var vel= 70
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if(vel>60){
+    console.log('Você ultrapassou a velocidade permitida, foi multado')
+}
