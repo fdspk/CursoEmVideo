d5b91968c4b35f2c1ae7ee3439d02d811056006e
Fabiana Kraft <fabianadspk@gmail.com>
2023-07-08 17:40:02 -0300

    Update script.js

--- Javascript/AULA 14 - EX01/script.js
+++ Javascript/AULA 14 - EX01/script.js
@@ -1,14 +1,29 @@
 function mostrar() {
     var res = document.getElementById('res')
     res.innerHTML = 'Contando:</br>'
-    var inicio = Number(document.getElementById('txtinicio').value)
-    var fim = Number(document.getElementById('txtfim').value)
-    var passo = Number(document.getElementById('txtpasso').value)
-    if (passo <= 0) {
+
+    var txtinicio=document.getElementById('txtinicio').value
+    var txtfim=document.getElementById('txtfim').value
+    var txtpasso=document.getElementById('txtpasso').value
+
+    var inicio = Number(txtinicio)
+    var fim = Number(txtfim)
+    var passo = Number(txtpasso)
+
+    if(txtinicio=='' || txtfim.length==0 || txtpasso==''){
+        alert('Inserir todos os valores')
+    }
+    else if (passo <= 0) {
         alert('Passo inválido: ponha valor maior que 0')
     }
-    else {
+    else if(inicio<=fim) {
         for (var cont = inicio; cont <= fim; cont += passo) {
+            res.innerHTML += `${cont} &#x27A1`
+        }
+        res.innerHTML+='&#x1F3F4'
+    }
+    else {
+        for (var cont = inicio; cont >=fim; cont -= passo) {
             res.innerHTML += `${cont} &#x27A1`
         }
         res.innerHTML+='&#x1F3F4'
